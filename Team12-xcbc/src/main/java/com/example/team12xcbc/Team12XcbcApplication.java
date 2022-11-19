2c3110c4b9bc3d82e875d2c48ab195b1995e701e
Dnaoatan1226 <819807093@qq.com>
2022-11-19 14:32:59 +0800

    后端框架

---
+++ Team12-xcbc/src/main/java/com/example/team12xcbc/Team12XcbcApplication.java
@@ -0,0 +1,13 @@
+package com.example.team12xcbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team12XcbcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Team12XcbcApplication.class, args);
+    }
+
+}
